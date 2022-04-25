2453d29a4497d2d5d79e9d3f02db334ce1fb31a0
nurjanahevs <evasitinurjanah2019@gmail.com>
2022-04-25 20:47:10 +0700

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AngularMaterialModule } from './angular-material.module';
+import { HeaderComponent } from './components/header/header.component';
+import { VideosComponent } from './components/videos/videos.component';
+import { PeopleComponent } from './components/people/people.component';
+import { DocumentsComponent } from './components/documents/documents.component';
+// import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    VideosComponent,
+    PeopleComponent,
+    DocumentsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AngularMaterialModule
+    // AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
